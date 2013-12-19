4d94e42569af9d7b8595214c0a6b553e1ee2bf69
Julien DERAY <julien@deray.fr>
2013-12-19 10:12:56 +0100

    Files size is now formatted (b, Kb, Mb, Gb)

--- app/model/ServerEntity.scala
+++ app/model/ServerEntity.scala
@@ -6,4 +6,13 @@ trait ServerEntity {
   val size: Long
   val icon: String
   val content: List[ServerEntity]
+
+  def formatedSize: String = {
+    size match {
+      case _ if size < 1000       => size + " b"
+      case _ if size < 1000000    => size/1000 + " Kb"
+      case _ if size < 1000000000 => size/1000000 + " Mb"
+      case _                      => size/1000000000 + " Gb"
+    }
+  }
 }
