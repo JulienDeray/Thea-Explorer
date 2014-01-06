7192d2a956e5c20377f7ae1365c4b107ce3076fb
Julien DERAY <julien@deray.fr>
2014-01-06 15:34:18 +0100

    Add authentification system

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,99 +1,74 @@
 package controllers
 
 import play.api.mvc._
-import core.Tools
-import java.io.{FileInputStream, File}
 import play.api.data.Form
 import play.api.data.Forms._
 import core.ConfigManager
-import scala.concurrent.ExecutionContext
-import ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object Application extends Controller {
 
   val config = new ConfigManager
 
-  val confForm = Form(
-    "rootPath" -> nonEmptyText
-  )
-
-  val launchInitForm = Form(
-    "status" -> nonEmptyText
-  )
-
-  def index = Action.async {
-    val futurFileSystem = scala.concurrent.Future { Tools.startBuildFileSystem( config("root-folder") ) }
-    futurFileSystem.map(
-      fileSystem => Ok( views.html.index( fileSystem ) )
+  val loginForm = Form(
+    tuple(
+      "user" -> nonEmptyText,
+      "password" -> nonEmptyText
     )
+  )
+
+  def login = Action { implicit request =>
+    request.session.get("user").map(
+      user => Redirect( "/splash" )
+    ).getOrElse( Ok( views.html.login() ) )
   }
 
-  def splash = Action {
-    Ok( views.html.splash() )
+  def logout = Action {
+    Redirect( "/" ).withNewSession
   }
 
-  def download(path: String) = Action {
-    Ok.sendFile( new File( config("root-folder") + "/" + Tools.unFormatFileUrl( path ) ) )
-  }
-
-  def downloadZip(path: String) = Action {
-    import play.api.libs.iteratee._
-    import java.util.zip._
-
-    val enumerator = Enumerator.outputStream { os =>
-      val rootPath = config("root-folder") + "/"
-      val zip = new ZipOutputStream( os )
-      val filelist = Tools.listFiles( rootPath + path )
-      val b = Array.fill[Byte](1024)(0)
-
-      for ( filePath <- filelist ) {
-        var count: Int = 1
-        val in = new FileInputStream( rootPath + Tools.unFormatFileUrl( filePath ) )
-
-        zip.putNextEntry( new ZipEntry( Tools.unFormatFileUrl( filePath ) ) )
-
-        while ( count > 0 ) {
-          zip.write(b, 0, count)
-          count = in.read(b)
+  def authentification = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        Ok( views.html.login() )
+      },
+      user => {
+        if ( user._1 == config("user") && user._2 == config("password") )
+          Redirect( "/splash" ).withSession( "user" -> user._1 )
+        else {
+          Ok( views.html.login() )
         }
-
-        zip.closeEntry()
-      }
-
-      zip.close()
-    }
-    val contentDisposition = "attachment; filename=" + path + ".zip"
-    Ok.chunked(enumerator >>> Enumerator.eof).withHeaders(
-      "Content-Type" -> "application/zip",
-      "Content-Disposition" -> contentDisposition
-    )
-  }
-
-  def setConf() = Action { implicit request =>
-    confForm.bindFromRequest().fold(
-      formWithErrors => BadRequest( "You have to post a 'root' value" ),
-      { root =>
-        config.saveProperties( config.addProperty("root-folder", root) )
-        Ok
       }
     )
   }
 
-  def launchInit() = Action { implicit request =>
-    launchInitForm.bindFromRequest().fold(
-      formWithErrors => BadRequest( "Serious issue !" ),
-      { status =>
-        Ok
-      }
-    )
+}
+
+
+/**
+ * Provide security features
+ */
+trait Secured {
+
+  /**
+   * Retrieve the connected user name.
+   */
+  private def user( request: RequestHeader ) = request.session.get("user")
+
+  /**
+   * Redirect to login if the user in not authorized.
+   */
+  private def onUnauthorized( request: RequestHeader ) = Results.Redirect( routes.Application.login( ) )
+
+  /**
+   * Action for authenticated users.
+   */
+  def IsAuthenticated( f: => String => Request[AnyContent] => Result ) = Security.Authenticated( user, onUnauthorized ) { user =>
+    Action( request => f( user )( request ) )
   }
 
-  def interpretedJS() = Action { implicit request =>
-    Ok( views.html.interpretedJS() )
-  }
-
-  def webSocketJS() = Action { implicit request =>
-    Ok( views.html.webSocket() )
+  def IsAuthenticatedAsync( f: => String => Request[AnyContent] => Future[SimpleResult] ) = Security.Authenticated( user, onUnauthorized ) { user =>
+    Action.async( request => f( user )( request ) )
   }
 
 }
---
+++ app/controllers/Dashboard.scala
@@ -0,0 +1,96 @@
+package controllers
+
+import play.api.mvc._
+import core.Tools
+import java.io.{FileInputStream, File}
+import play.api.data.Form
+import play.api.data.Forms._
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
+
+
+object Dashboard extends Controller with Secured {
+
+  val confForm = Form(
+    "rootPath" -> nonEmptyText
+  )
+
+  val launchInitForm = Form(
+    "status" -> nonEmptyText
+  )
+
+  def dashboard = IsAuthenticatedAsync { user => _ =>
+    val futurFileSystem = scala.concurrent.Future { Tools.startBuildFileSystem( Application.config("root-folder") ) }
+    futurFileSystem.map(
+      fileSystem => Ok( views.html.index( fileSystem ) )
+    )
+  }
+
+  def splash = IsAuthenticated { user => _ =>
+      Ok( views.html.splash() )
+  }
+
+  def download(path: String) = Action {
+    Ok.sendFile( new File( Application.config("root-folder") + "/" + Tools.unFormatFileUrl( path ) ) )
+  }
+
+  def downloadZip(path: String) = Action {
+    import play.api.libs.iteratee._
+    import java.util.zip._
+
+    val enumerator = Enumerator.outputStream { os =>
+      val rootPath = Application.config("root-folder") + "/"
+      val zip = new ZipOutputStream( os )
+      val filelist = Tools.listFiles( rootPath + path )
+      val b = Array.fill[Byte](1024)(0)
+
+      for ( filePath <- filelist ) {
+        var count: Int = 1
+        val in = new FileInputStream( rootPath + Tools.unFormatFileUrl( filePath ) )
+
+        zip.putNextEntry( new ZipEntry( Tools.unFormatFileUrl( filePath ) ) )
+
+        while ( count > 0 ) {
+          zip.write(b, 0, count)
+          count = in.read(b)
+        }
+
+        zip.closeEntry()
+      }
+
+      zip.close()
+    }
+    val contentDisposition = "attachment; filename=" + path + ".zip"
+    Ok.chunked(enumerator >>> Enumerator.eof).withHeaders(
+      "Content-Type" -> "application/zip",
+      "Content-Disposition" -> contentDisposition
+    )
+  }
+
+  def setConf() = Action { implicit request =>
+    confForm.bindFromRequest().fold(
+    formWithErrors => BadRequest( "You have to post a 'root' value" ),
+    { root =>
+      Application.config.saveProperties( Application.config.addProperty("root-folder", root) )
+      Ok
+    }
+    )
+  }
+
+  def launchInit() = Action { implicit request =>
+    launchInitForm.bindFromRequest().fold(
+    formWithErrors => BadRequest( "Serious issue !" ),
+    { status =>
+      Ok
+    }
+    )
+  }
+
+  def interpretedJS() = Action { implicit request =>
+    Ok( views.html.interpretedJS() )
+  }
+
+  def webSocketJS() = Action { implicit request =>
+    Ok( views.html.webSocket() )
+  }
+}
